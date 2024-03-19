982c253a0e9204af764feb42f96bb3802157488c
vmfhdirn2 <vmfhdirn2@kakao.com>
2024-03-20 03:40:18 +0900

    2장 완료(게시판 기초공사 완료)

--- src/main/java/com/mysite/sbb/answer/AnswerForm.java
+++ src/main/java/com/mysite/sbb/answer/AnswerForm.java
@@ -1,5 +1,12 @@
 package com.mysite.sbb.answer;
 
-public class AnswerForm {
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class AnswerForm {
+		@NotEmpty(message = "내용은 필수 항목입니다.")
+		private String content;
 }
